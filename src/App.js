7d99fe2371ba82ce2ffaf78f050de156c2e34f10
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-03 11:25:29 -0500

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Weather from "./Weather.js";
+import Footer from "./Footer.js";
 import "./App.css";
 
 function App() {
@@ -7,16 +8,7 @@ function App() {
     <div className="App">
       <h1> Weather App </h1>
       <Weather />
-      <footer>
-        <a
-          href="https://github.com/brigittex/week4-homework"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Brigitte Giguère <i className="fab fa-canadian-maple-leaf"></i>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/brigittex/week4-homework"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Brigitte Giguère <i className="fab fa-canadian-maple-leaf"></i>
+      </footer>
+    </div>
+  );
+}
