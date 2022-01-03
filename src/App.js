9675c0802744e593eef3fd50f312ca688f26dab2
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-03 11:16:08 -0500

    Added GitHub and Netlify links, removed default page content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/brigittex/week4-homework"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Open-source code
+        </a>{" "}
+        by Brigitte Giguère <i className="fab fa-canadian-maple-leaf"></i>
+      </footer>
     </div>
   );
 }
